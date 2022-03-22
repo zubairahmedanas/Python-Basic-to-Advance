bb6b9a140f76f09fcc1b44087d7866aa9682d2e4
zubairahmedanas <amianasahmed@gmail.com>
2022-03-22 17:38:25 +0600

    discount calculation

--- prac_1.py
+++ prac_1.py
@@ -1,9 +1,28 @@
-num = int(input("Enter a number: "))
-# num1 = int(num)
-print(num)
-print(type(num))
+# num = int(input("Enter a number: "))
+# # num1 = int(num)
+# print(num)
+# print(type(num))
+#
+# num = float(input("Enter a number: "))
+# # num1 = int(num)
+# print(num)
+# print(type(num))
 
-num = float(input("Enter a number: "))
-# num1 = int(num)
-print(num)
-print(type(num))
+
+
+# x  = 5
+# quotient = x//2
+# reminder = x%2
+# print('quotient is: ', quotient)
+# print('remindre is:', reminder)
+
+
+fee = 4535
+
+discount = 10
+
+get_dis = (discount/100)* fee
+
+print(get_dis)
+amount_after_discount =  fee-get_dis
+print(amount_after_discount)
