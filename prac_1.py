f65253505ad80c2d1b50600a605a88214f5c5a46
zubairahmedanas <amianasahmed@gmail.com>
2022-03-22 15:24:49 +0600

    int and float input

--- prac_1.py
+++ prac_1.py
@@ -1,3 +1,9 @@
-a= 5
-b= 5
-print(a+b)
+num = int(input("Enter a number: "))
+# num1 = int(num)
+print(num)
+print(type(num))
+
+num = float(input("Enter a number: "))
+# num1 = int(num)
+print(num)
+print(type(num))
