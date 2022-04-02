5dcf079ff8cd63195af67c6861c5596398bfec70
zubairahmedanas <amianasahmed@gmail.com>
2022-04-02 16:10:42 +0600

    object oriented add

--- prac_1.py
+++ prac_1.py
@@ -62,10 +62,61 @@
 
 p1 = {"name": "anas", "dept": "cse"}
 # print(p1.get("institute", ["NC","bits",'ti']))
-p1["institute"]="bits","NC"
+p1["institute"] = "bits", "NC"
+
 
 # print(p1)
 # p1.pop("dept")
 # print(p1)
-for key in p1:
-    print(p1[key])
+# for key in p1:
+#     print(p1[key])
+
+# class Student:
+#     def chk_pass_fail(self):
+#         if self.marks <= 40:
+#             return False
+#         else:
+#             return True
+#     def __init__(self, name, marks):
+#         self.name = name
+#         self.marks = marks
+#
+#
+# student_1 = Student("Anas", 80)
+# # print(student_1.marks)
+# student_2 = Student("Zubair",35)
+#
+# res = student_1.chk_pass_fail()
+# print(res)
+# res = student_2.chk_pass_fail()
+# print(res)
+# student_1.name ="max"
+# student_1.marks = 85
+#
+# result = student_1.chk_pass_fail()
+# print(result)
+#
+# student_2 =Student()
+# student_2.name ="clasrk"
+# student_2.marks = 33
+# result = student_2.chk_pass_fail()
+# print(result)
+class Complex:
+
+
+    def __init__(self, real ,imag):
+        self.real =real
+        self.imag =imag
+
+    def add(self, number):
+        real = self.real+ number.real
+        imag = self.imag+ number.imag
+        result = Complex(real,imag)
+        return  result
+
+
+n1 = Complex(5,4)
+n2 = Complex(4, -3)
+result = n1.add(n2)
+print(result.real)
+print(result.imag)
